bd43e2b7737a27dd34133e9af261f9b94e400368
sinanozlen <157057982+sinanozlen@users.noreply.github.com>
2025-02-05 01:29:24 +0300

    "mongoconnected"

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors")
+const connection = require("./database/db");
+
+
+
+app.use(express.json);
+app.use(cors());
+
+
+connection();
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Uygulama http://localhost:5000 portunda ayaga kalkti"));

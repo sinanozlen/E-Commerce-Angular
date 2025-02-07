1e1057686a286117929da78e065849bca543fd3d
sinanozlen <157057982+sinanozlen@users.noreply.github.com>
2025-02-07 12:14:19 +0300

    NgxSpinnerAdded

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import {RouterModule} from '@angular/router'
+import { NgxSpinnerModule } from 'ngx-spinner';
  
 @Component({
   selector: 'app-root',  
-  template: "<router-outlet></router-outlet>",
+  template: `<router-outlet></router-outlet>
+  <ngx-spinner bdColor = "rgba(0, 0, 0, 0.8)" size = "medium" 
+  color = "#fff" type = "ball-clip-rotate-multiple" 
+  [fullScreen] = "true">
+  <p style="color: white" > Sayfa Açılıyor Bekleyiniz... </p></ngx-spinner>`,
   standalone:true,
-  imports:[RouterModule]
+  imports:[RouterModule,NgxSpinnerModule]
   
 })
 export class AppComponent {}
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../common/shared/shared.module';
 import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +12,17 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent {
 
+  constructor(
+    private _toastr: ToastrService,
+    private _spinner: NgxSpinnerService
+  )
+ {
+  this._spinner.show();
+  setTimeout(() =>{
+    this._spinner.hide()
+  },5000)
+ }
+  
   login(form:NgForm){
     if(form.valid){
       console.log(form.value)
